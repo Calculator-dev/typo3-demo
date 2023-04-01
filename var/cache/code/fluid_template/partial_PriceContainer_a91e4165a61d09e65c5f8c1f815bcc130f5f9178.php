6ca204236f39acf8fc4e657b5e2b115fe1ceeef7
calculator-dev <haris.aksalic@hotmail.com>
2023-04-01 05:23:00 +0200

    Added price container that is dynamical, based on arguments that are passed as a props

---
+++ var/cache/code/fluid_template/partial_PriceContainer_a91e4165a61d09e65c5f8c1f815bcc130f5f9178.php
@@ -0,0 +1,105 @@
+<?php
+
+class partial_PriceContainer_a91e4165a61d09e65c5f8c1f815bcc130f5f9178 extends \TYPO3Fluid\Fluid\Core\Compiler\AbstractCompiledTemplate {
+
+public function getLayoutName(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+return (string) '';
+}
+public function hasLayout() {
+return FALSE;
+}
+public function addCompiledNamespaces(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$renderingContext->getViewHelperResolver()->addNamespaces(array (
+  'core' => 
+  array (
+    0 => 'TYPO3\\CMS\\Core\\ViewHelpers',
+  ),
+  'f' => 
+  array (
+    0 => 'TYPO3Fluid\\Fluid\\ViewHelpers',
+    1 => 'TYPO3\\CMS\\Fluid\\ViewHelpers',
+  ),
+  'formvh' => 
+  array (
+    0 => 'TYPO3\\CMS\\Form\\ViewHelpers',
+  ),
+));
+}
+
+/**
+ * Main Render function
+ */
+public function render(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+$output0 = '';
+
+$output0 .= '<div
+      class="price position-absolute d-flex flex-row justify-content-center gap-10 align-items-center"
+    >
+      <div class="price-header position-absolute btn-orange d-inline-block">
+        Nur Für Kurze Zeit
+      </div>
+      ';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\IfViewHelper
+$renderChildrenClosure2 = function() use ($renderingContext, $self) {
+$output6 = '';
+
+$output6 .= '
+        <div class="old-price-container d-inline-flex flex-column text-white">
+          <p class="m-0">statt</p>
+          <p class="old-price-tag m-0">';
+$array7 = array (
+);
+$output6 .= call_user_func_array( function ($var) { return (is_string($var) || (is_object($var) && method_exists($var, '__toString')) ? htmlspecialchars((string) $var, ENT_QUOTES) : $var); }, [$renderingContext->getVariableProvider()->getByPath('oldPrice', $array7)]);
+
+$output6 .= ',90 €</p>
+        </div>
+      ';
+return $output6;
+};
+$arguments1 = array();
+$arguments1['then'] = NULL;
+$arguments1['else'] = NULL;
+$arguments1['condition'] = false;
+// Rendering Boolean node
+// Rendering Array
+$array3 = array();
+$array4 = array (
+);$array3['0'] = $renderingContext->getVariableProvider()->getByPath('oldPrice', $array4);
+
+$expression5 = function($context) {return TYPO3Fluid\Fluid\Core\Parser\BooleanParser::convertNodeToBoolean($context["node0"]);};
+$arguments1['condition'] = TYPO3Fluid\Fluid\Core\Parser\SyntaxTree\BooleanNode::convertToBoolean(
+					$expression5(
+						TYPO3Fluid\Fluid\Core\Parser\SyntaxTree\BooleanNode::gatherContext($renderingContext, $array3)
+					),
+					$renderingContext
+				);
+$arguments1['__thenClosure'] = $renderChildrenClosure2;
+
+$output0 .= TYPO3Fluid\Fluid\ViewHelpers\IfViewHelper::renderStatic($arguments1, $renderChildrenClosure2, $renderingContext);
+
+$output0 .= '
+      <div class="text-white position-relative d-inline-flex flex-row">
+        <p class="m-0 lg-letter font-weight-bold">';
+$array8 = array (
+);
+$output0 .= call_user_func_array( function ($var) { return (is_string($var) || (is_object($var) && method_exists($var, '__toString')) ? htmlspecialchars((string) $var, ENT_QUOTES) : $var); }, [$renderingContext->getVariableProvider()->getByPath('price', $array8)]);
+
+$output0 .= ',</p>
+        <div
+          class="d-inline-flex position-absolute flex-column secondary-price"
+        >
+          <span class="md-letter font-weight-bold">90</span>
+          <p class="m-0 sm-letter">€ mb</p>
+        </div>
+      </div>
+    </div>
+  </div>';
+
+return $output0;
+}
+
+
+}
+#
